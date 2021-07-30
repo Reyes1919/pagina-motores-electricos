84bfbef715bb6e4c98f32aacaa25c7372d628e5b
Reyes1919 <88211857+Reyes1919@users.noreply.github.com>
2021-07-30 16:26:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+
+ScrollReveal().reveal('.showcase');
+
+ScrollReveal().reveal('.contenido', { delay: 500});
+
+ScrollReveal().reveal('.banner', { delay: 500});
